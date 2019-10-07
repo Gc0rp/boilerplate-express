f03590c3e083162091286565beb68aee5d77cdd1
Gc0rp <ojasgupta4321@gmail.com>
2019-10-07 12:19:34 +1100

    Added task 4, 5, and 6.

--- myApp.js
+++ myApp.js
@@ -9,22 +9,36 @@ var app = express();
 
 
 /** 1) Meet the node console. */
-console.log("Hello World");
+console.log("Hello World")
 
 /** 2) A first working Express Server */
 
+// app.get("/", function(req, res) {
+//   res.send("Hello Express")
+// });
 
 /** 3) Serve an HTML file */
-
+let absolutePath = __dirname + '/views/index.html'
+app.get('/', function(req, res){
+  res.sendFile(absolutePath);
+})  
 
 /** 4) Serve static assets  */
+// app.use(express.static(__dirname + '/public'));
 
 
 /** 5) serve JSON on a specific route */
-
+// app.get('/json', (req,res) => {res.json({"message" : "Hello json"})});
 
 /** 6) Use the .env file to configure the app */
- 
+
+app.get('/json', (req, res) => {
+  let data = {"message": "Hello json"};
+  if(process.env.MESSAGE_STYLE === 'uppercase'){
+    data.message = data.message.toUpperCase();
+  }
+  res.json(data);
+});
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
